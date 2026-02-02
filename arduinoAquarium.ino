081e9f32d07561791b842426f90c92da15ae033e
banan-guh <leon.chen.lop@gmail.com>
2026-02-02 09:39:50 -0800

    buh

--- arduinoAquarium.ino
+++ arduinoAquarium.ino
@@ -1,8 +1,12 @@
 const int threshold = 900;
 const int greenLED = 3;
-const int redLED = 4;
-// A5, A7
-
+const int redLED =   4;
+// Too high floats: A5 (upper), A7 (lower)
+// Too low floats:  A1 (upper), A# (lower)
+const int pinHighTop =    A5;
+const int pinHighBottom = A7;
+//const int pinLowTop =     A1;
+//const int pinLowBottom =  A#;
 
 void do_LED(bool isOK) {
   if (isOK) {
@@ -22,8 +26,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int readOne = analogRead(A5);
-  int readTwo = analogRead(A7);
+  int readOne = digitalRead(A5);
+  int readTwo = digitalRead(A7);
+  //int readThree = analogRead();
+  //int readFour = analogRead();
 
   bool isOneTriggered = (readOne < threshold);
   bool isTwoTriggered = (readTwo < threshold);
