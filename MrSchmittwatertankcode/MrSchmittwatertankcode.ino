9c31cfe3504abf9d418b0de94430f5401953cf2b
carter-sys <carter.klein.stu@learn.sd23.bc.ca>
2026-02-04 10:34:51 -0800

    this will probably suck

--- MrSchmittwatertankcode/MrSchmittwatertankcode.ino
+++ MrSchmittwatertankcode/MrSchmittwatertankcode.ino
@@ -3,28 +3,44 @@ const int greenLED = 3;
 const int redLED =   4;
 const int floatOne = 5;
 const int floatTwo = 6;
+const int MotorHigh = 11;
+const int MotorLow = 12;
+const int button = 7;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(greenLED, OUTPUT);
   pinMode(redLED,   OUTPUT);
+  pinMode(MotorHigh, OUTPUT);
+  pinMode(MotorLow, OUTPUT);
   
   pinMode(floatTwo, INPUT_PULLUP);
   pinMode(floatOne, INPUT_PULLUP);
+  pinMode(button, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int readOne = digitalRead(floatOne);
   int readTwo = digitalRead(floatTwo);
-
+  int buttonread = digitalRead(button);
   bool isFloatOneUp = (readOne == LOW);
   bool isFloatTwoUp = (readTwo == LOW);
-  
+  bool isbuttonread = (buttonread == LOW);
   //Serial.println(isFloatOneUp);
   //Serial.println(isFloatTwoUp);
-
+ 
+  if (isbuttonread) {
+      digitalWrite(MotorHigh, HIGH);
+      digitalWrite(MotorLow, LOW);
+ }else{
+      digitalWrite(MotorHigh, LOW);
+      digitalWrite(MotorLow, LOW);
+ }
+   
+  
   if (isFloatOneUp and isFloatTwoUp) {
     digitalWrite(redLED,  HIGH);
     digitalWrite(greenLED, LOW); 
